284e211c04b9286b81d54822487c5c3ac7c315fd
Eran Hirsch <hirscheran@gmail.com>
2022-04-17 12:12:18 +0300

    Change UI based on latest feedback

---
+++ union_annotation/src/components/highlight_tooltip.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
+
+
+function HighlightTooltip({text, tooltipText}) {
+    /*
+    Tooltip class wrapper for highlighting needs
+    */
+
+    const renderTooltip = (props) => (
+        <Tooltip id="button-tooltip" {...props}>
+          {Array.isArray(tooltipText) ? tooltipText.map(tooltipMsg => <span>* {tooltipMsg}<br/></span>) : tooltipText}
+        </Tooltip>
+      );
+
+    return <OverlayTrigger
+                placement="top"
+                delay={{ show: 250, hide: 400 }}
+                overlay={renderTooltip}
+            >
+        <span className="tooltip-available">{text}</span>
+    </OverlayTrigger>
+    
+}
+
+export { HighlightTooltip };
